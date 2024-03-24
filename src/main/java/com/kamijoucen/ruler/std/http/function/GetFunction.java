d6a6ed9251869362993fd99e8d5ae9b2db026e3f
kamijoucen <kamijoucen@foxmail.com>
2024-03-24 23:35:56 +0800

    params

--- src/main/java/com/kamijoucen/ruler/std/http/function/GetFunction.java
+++ src/main/java/com/kamijoucen/ruler/std/http/function/GetFunction.java
@@ -1,9 +1,11 @@
 package com.kamijoucen.ruler.std.http.function;
 
+import java.util.Map;
 import com.kamijoucen.ruler.function.RulerFunction;
 import com.kamijoucen.ruler.runtime.RuntimeContext;
 import com.kamijoucen.ruler.runtime.Scope;
 import com.kamijoucen.ruler.std.http.HttpClient;
+import com.kamijoucen.ruler.util.CollectionUtil;
 import com.kamijoucen.ruler.value.BaseValue;
 
 public class GetFunction implements RulerFunction {
@@ -16,12 +18,37 @@ public class GetFunction implements RulerFunction {
         if (!(params[0] instanceof String)) {
             throw new RuntimeException("Get function need a string param.");
         }
-        return HttpClient.get(params[0].toString());
+        String url = params[0].toString();
+        if (params.length == 2) {
+            url += buildUrlParams(params[1]);
+        }
+        return HttpClient.get(url);
     }
 
     @Override
     public String getName() {
         return "Get";
+    }
+
+    private String buildUrlParams(Object obj) {
+        if (obj == null) {
+            return "";
+        }
+        if (obj instanceof String) {
+            return obj.toString().startsWith("?") ? obj.toString() : "?" + obj.toString();
+        }
+        if (!(obj instanceof Map<?, ?>)) {
+            return "";
+        }
+        Map<?, ?> map = (Map<?, ?>) obj;
+        if (CollectionUtil.isEmpty(map)) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder("?");
+        for (Map.Entry<?, ?> entry : map.entrySet()) {
+            sb.append(entry.getKey()).append("=").append(entry.getValue()).append("&");
+        }
+        return sb.substring(0, sb.length() - 1);
     }
 
 }
